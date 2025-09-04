73318d6f7c2481c7979fab837b1bf9116c145660
Imani Dinkins <izdinkins@aggies.ncat.edu>
2025-09-04 13:24:42 -0400

    rootfix

--- src/components/sections/Resume.jsx
+++ src/components/sections/Resume.jsx
@@ -12,7 +12,7 @@ const Resume = () => {
       <h1 className="text-4xl font-bold text-white mb-6">My Resume</h1>
 
       <iframe
-        src="public/ImaniDinkins_Resume_Fall2025_Online.pdf"
+        src="/ImaniDinkins_Resume_Fall2025_Online.pdf"
         title="Resume"
         className="w-full max-w-4xl h-[800px] border border-gray-700 rounded"
       ></iframe>
